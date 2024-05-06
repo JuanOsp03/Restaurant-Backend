396d48f5d4abd7872ce065b0ee2818202ee6075f
JuanOsp03 <ospinaj953@gmail.com>
2024-05-06 11:54:47 -0500

    GET RESTAURANT

--- src/Routers/restaurantrouter.js
+++ src/Routers/restaurantrouter.js
@@ -7,5 +7,6 @@ router.get('/listrestaurant', restaurantController.listRestaurant);
 router.put('/updaterestaurant/:restaurantId', restaurantController.updateRestaurant);
 router.put('/disablerestaurant/:restaurantId', restaurantController.disableRestaurant);
 router.put('/enablerestaurant/:restaurantId', restaurantController.enableRestaurant);
+router.put('/getrestaurant/:restaurantId', restaurantController.getRestaurant);
 
 module.exports = router;
